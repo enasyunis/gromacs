e14656873081e854237063b23342344df7cb9b06
Enas Yunis <enas.yunis@kaust.edu.sa>
2013-04-28 12:12:32 +0300

    cleaned up ebin.c

--- src/mdlib/ebin.c
+++ src/mdlib/ebin.c
@@ -1,40 +1,3 @@
-/*
- * This file is part of the GROMACS molecular simulation package.
- *
- * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
- * Copyright (c) 2001-2004, The GROMACS development team,
- * check out http://www.gromacs.org for more information.
- * Copyright (c) 2012,2013, by the GROMACS development team, led by
- * David van der Spoel, Berk Hess, Erik Lindahl, and including many
- * others, as listed in the AUTHORS file in the top-level source
- * directory and at http://www.gromacs.org.
- *
- * GROMACS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * GROMACS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with GROMACS; if not, see
- * http://www.gnu.org/licenses, or write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
- *
- * If you want to redistribute modifications to GROMACS, please
- * consider that scientific software is very special. Version
- * control is crucial - bugs must be traceable. We will be happy to
- * consider code for inclusion in the official distribution, but
- * derived work must not be called official GROMACS. Details are found
- * in the README & COPYING files - if they are missing, get the
- * official version at http://www.gromacs.org.
- *
- * To help us fund GROMACS development, we humbly ask that you cite
- * the research papers on the package. Check out http://www.gromacs.org.
- */
 /* This file is completely threadsafe - keep it that way! */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -54,7 +17,7 @@
 #include "physics.h"
 
 t_ebin *mk_ebin(void)
-{
+{ // called  
     t_ebin *eb;
 
     snew(eb, 1);
@@ -63,7 +26,7 @@ t_ebin *mk_ebin(void)
 }
 
 int get_ebin_space(t_ebin *eb, int nener, const char *enm[], const char *unit)
-{
+{ // called  
     int         index;
     int         i, f;
     const char *u;
@@ -73,7 +36,7 @@ int get_ebin_space(t_ebin *eb, int nener, const char *enm[], const char *unit)
     srenew(eb->e, eb->nener);
     srenew(eb->e_sim, eb->nener);
     srenew(eb->enm, eb->nener);
-    for (i = index; (i < eb->nener); i++)
+    for (i = index; (i < eb->nener); i++) // (index,nener): (0,9), (9,18), (18,27), (27,28), (28,29), (29,30)
     {
         eb->e[i].e        = 0;
         eb->e[i].eav      = 0;
@@ -82,109 +45,52 @@ int get_ebin_space(t_ebin *eb, int nener, const char *enm[], const char *unit)
         eb->e_sim[i].eav  = 0;
         eb->e_sim[i].esum = 0;
         eb->enm[i].name   = strdup(enm[i-index]);
-        if (unit != NULL)
+        if (unit != NULL) 
         {
             eb->enm[i].unit = strdup(unit);
         }
-        else
-        {
-            /* Determine the unit from the longname.
-             * These units should have been defined in ifunc.c
-             * But even better would be if all interactions functions
-             * return energies and all non-interaction function
-             * entries would be removed from the ifunc array.
-             */
-            u = unit_energy;
-            for (f = 0; f < F_NRE; f++)
-            {
-                if (strcmp(eb->enm[i].name,
-                           interaction_function[f].longname) == 0)
-                {
-                    /* Only the terms in this list are not energies */
-                    switch (f)
-                    {
-                        case F_DISRESVIOL: u = unit_length;   break;
-                        case F_ORIRESDEV:  u = "obs";         break;
-                        case F_TEMP:       u = unit_temp_K;   break;
-                        case F_PDISPCORR:
-                        case F_PRES:       u = unit_pres_bar; break;
-                    }
-                }
-            }
-            eb->enm[i].unit = strdup(u);
-        }
     }
-
+    if (eb->nener == 9) {
+          eb->enm[7].unit = strdup(unit_temp_K);
+          eb->enm[8].unit = strdup(unit_pres_bar);
+    }
     return index;
 }
 
 void add_ebin(t_ebin *eb, int index, int nener, real ener[], gmx_bool bSum)
-{
+{ // called  
     int       i, m;
     double    e, sum, sigma, invmm, diff;
     t_energy *eg, *egs;
 
-    if ((index+nener > eb->nener) || (index < 0))
-    {
-        gmx_fatal(FARGS, "%s-%d: Energies out of range: index=%d nener=%d maxener=%d",
-                  __FILE__, __LINE__, index, nener, eb->nener);
-    }
 
     eg = &(eb->e[index]);
-
-    for (i = 0; (i < nener); i++)
+    for (i = 0; (i < nener); i++) // three 9's followed by three 1's
     {
         eg[i].e      = ener[i];
     }
+    egs = &(eb->e_sim[index]);
 
-    if (bSum)
+    m = eb->nsum;
+
+    for (i = 0; (i < nener); i++)
     {
-        egs = &(eb->e_sim[index]);
-
-        m = eb->nsum;
-
-        if (m == 0)
-        {
-            for (i = 0; (i < nener); i++)
-            {
-                eg[i].eav    = 0;
-                eg[i].esum   = ener[i];
-                egs[i].esum += ener[i];
-            }
-        }
-        else
-        {
-            invmm = (1.0/(double)m)/((double)m+1.0);
-
-            for (i = 0; (i < nener); i++)
-            {
-                /* Value for this component */
-                e = ener[i];
-
-                /* first update sigma, then sum */
-                diff         = eg[i].esum - m*e;
-                eg[i].eav   += diff*diff*invmm;
-                eg[i].esum  += e;
-                egs[i].esum += e;
-            }
-        }
+         eg[i].eav    = 0;
+         eg[i].esum   = ener[i];
+         egs[i].esum += ener[i];
     }
 }
 
 void ebin_increase_count(t_ebin *eb, gmx_bool bSum)
-{
+{// called  
     eb->nsteps++;
     eb->nsteps_sim++;
-
-    if (bSum)
-    {
-        eb->nsum++;
-        eb->nsum_sim++;
-    }
+    eb->nsum++;
+    eb->nsum_sim++;
 }
 
 void reset_ebin_sums(t_ebin *eb)
-{
+{// called  
     eb->nsteps = 0;
     eb->nsum   = 0;
     /* The actual sums are cleared when the next frame is stored */
@@ -192,29 +98,17 @@ void reset_ebin_sums(t_ebin *eb)
 
 void pr_ebin(FILE *fp, t_ebin *eb, int index, int nener, int nperline,
              int prmode, gmx_bool bPrHead)
-{
+{ // called 
     int  i, j, i0;
     real ee = 0;
     int  rc;
     char buf[30];
 
     rc = 0;
-
-    if (index < 0)
+    nener = index + nener;
+    for (i = index; (i < nener) && rc >= 0; ) // nener=9 called four times.
     {
-        gmx_fatal(FARGS, "Invalid index in pr_ebin: %d", index);
-    }
-    if (nener == -1)
-    {
-        nener = eb->nener;
-    }
-    else
-    {
-        nener = index + nener;
-    }
-    for (i = index; (i < nener) && rc >= 0; )
-    {
-        if (bPrHead)
+        if (bPrHead) // true on the first two calls
         {
             i0 = i;
             for (j = 0; (j < nperline) && (i < nener) && rc >= 0; j++, i++)
@@ -255,58 +149,5 @@ void pr_ebin(FILE *fp, t_ebin *eb, int index, int nener, int nperline,
             rc = fprintf(fp, "\n");
         }
     }
-    if (rc < 0)
-    {
-        gmx_fatal(FARGS, "Cannot write to logfile; maybe you are out of disk space?");
-    }
 }
 
-#ifdef DEBUGEBIN
-int main(int argc, char *argv[])
-{
-#define NE 12
-#define NT 7
-#define NS 5
-
-    t_ebin *eb;
-    int     i;
-    char    buf[25];
-    char   *ce[NE], *ct[NT], *cs[NS];
-    real    e[NE], t[NT], s[NS];
-    int     ie, it, is;
-
-    eb = mk_ebin();
-    for (i = 0; (i < NE); i++)
-    {
-        e[i] = i;
-        sprintf(buf, "e%d", i);
-        ce[i] = strdup(buf);
-    }
-    ie = get_ebin_space(eb, NE, ce);
-    add_ebin(eb, ie, NE, e, 0);
-    for (i = 0; (i < NS); i++)
-    {
-        s[i] = i;
-        sprintf(buf, "s%d", i);
-        cs[i] = strdup(buf);
-    }
-    is = get_ebin_space(eb, NS, cs);
-    add_ebin(eb, is, NS, s, 0);
-    for (i = 0; (i < NT); i++)
-    {
-        t[i] = i;
-        sprintf(buf, "t%d", i);
-        ct[i] = strdup(buf);
-    }
-    it = get_ebin_space(eb, NT, ct);
-    add_ebin(eb, it, NT, t, 0);
-
-    printf("Normal:\n");
-    pr_ebin(stdout, eb, 0, -1, 5, eprNORMAL, 1);
-
-    printf("Average:\n");
-    pr_ebin(stdout, eb, ie, NE, 5, eprAVER, 1);
-    pr_ebin(stdout, eb, is, NS, 3, eprAVER, 1);
-    pr_ebin(stdout, eb, it, NT, 4, eprAVER, 1);
-}
-#endif
